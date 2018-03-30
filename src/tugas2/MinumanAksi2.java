9550b0e15998c9695e2a61ed4a69971c3b28f99e
galuhnurdianty <32261792+galuhnurdianty@users.noreply.github.com>
2018-03-31 00:23:53 +0800

    Mengisi Nilai Atribut

--- src/tugas2/MinumanAksi2.java
+++ src/tugas2/MinumanAksi2.java
@@ -2,5 +2,9 @@ package tugas2;
 public class MinumanAksi2 {
     public static void main(String[] args) {
         Minuman2 n1 = new Minuman2();
+        System.out.println("    _______________________");
+        System.out.println("    |MENGHITUNG PENDAPATAN|");
+        n1.harga = 17000;
+        n1.jumlah = 10;
     }
 }
