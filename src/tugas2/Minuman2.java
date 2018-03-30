3930616d38e2f54389d77e4b8797b5e549204e7a
galuhnurdianty <32261792+galuhnurdianty@users.noreply.github.com>
2018-03-31 00:15:17 +0800

    Membuat Atribut dan Behavior

--- src/tugas2/Minuman2.java
+++ src/tugas2/Minuman2.java
@@ -1,4 +1,11 @@
 package tugas2;
 public class Minuman2 {
+    double harga;
+    double jumlah;
     
+    void cetakInfo(){
+    System.out.println("==================================");        
+    System.out.println("Harga Minuman           :"+harga);
+    System.out.println("Jumlah Minuman Terjual  :"+jumlah);
+    }
 }
